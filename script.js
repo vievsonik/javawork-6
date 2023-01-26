8dbb0d565d1fd0cf71c7b45c6341a4b79fc7b5d2
vievsonik <118842811+vievsonik@users.noreply.github.com>
2023-01-26 15:53:24 +0400

    savee

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 // task1
 let factory = {
-    facName : 'Volkswagen Wolfsburg Plant',
-    calculateWorkload : function (){
-        const employeWorkData = this.employeeWeeklyWorkload;
-        const workResultdata = [];
-        if(employeWorkData && employeWorkData.length > 0){
-            for(let emp of employeWorkData){
-                    workResultdata.push(emp.workload)
-            }
-            return workResultdata
-        }else "araa swori zmao"
-    },
-    formatArray : function (name) {
+  facName: "nissan",
+  calculateWorkload: function () {
+    const employeWorkData = this.employeeWeeklyWorkload;
+    const workResultdata = [];
+    if (employeWorkData && employeWorkData.length > 0) {
+      for (let emp of employeWorkData) {
+        workResultdata.push(emp.workload || emp.timeSpent);
+      }
+      return workResultdata;
+    } else "araa swori";
+  },
+  formatArray: function (name) {},
+};
 
-    }
-    
-}
-
-// let factoryEmployees = {
-//     employees : [{name : "John", timeSpent : 1 } , {name : "Sam", timeSpent : 3 },{name : "Maria", timeSpent : 2 },{name : "Dan", timeSpent : 4 },{name : "Lorelai", timeSpent : 5 }],
-//     employeeNum : this.employees.length,
-// }
+let factoryEmployees = {
+  employees: [
+    { name: "John", timeSpent: 1 },
+    { name: "Sam", timeSpent: 3 },
+    { name: "Maria", timeSpent: 2 },
+    { name: "Dan", timeSpent: 4 },
+    { name: "Lorelai", timeSpent: 5 },
+  ],
+  // employeeNum : this.employees.length,
+};
 
 let workloadData = {
-    employeeWeeklyWorkload : [{name : "John", workload : 40 } , {name : "Sam", timeSpent : 25 },{name : "Maria", timeSpent : 28 },{name : "Dan", timeSpent : 30 },{name : "Lorelai", timeSpent : 31 }]
-}
-let workArr = this.factory.calculateWorkload.bind(workloadData)();
+  employeeWeeklyWorkload: [
+    { name: "John", workload: 40 },
+    { name: "Sam", timeSpent: 25 },
+    { name: "Maria", timeSpent: 28 },
+    { name: "Dan", timeSpent: 30 },
+    { name: "Lorelai", timeSpent: 31 },
+  ],
+};
 
-console.log (workArr) 
+let workArr = factory.calculateWorkload.bind(workloadData)();
 
+console.log(workArr);
 
 // task 6
 // let arr2 = [2,2,2,1,30]
@@ -37,7 +46,5 @@ console.log (workArr)
 // return arr2.filter((item,
 // index) => arr.indexOf(item) === index);
 // }
-
-
 
 // console.log(removeDuplicates(arr2));
